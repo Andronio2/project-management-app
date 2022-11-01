f20128cba8860949f9366618c842da7700894f38
Aibek <ai.bek@mail.ru>
2022-11-01 23:34:56 +0600

    feat: add saving token in local storage

--- project-management/src/app/core/services/API/auth.service.ts
+++ project-management/src/app/core/services/API/auth.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BASEURL } from 'src/app/share/constants/constants';
+import { Observable, tap } from 'rxjs';
+import { BASEURL, TOKEN_KEY } from 'src/app/share/constants/constants';
 import { IToken, IUser } from 'src/app/share/models/auth.model';
 import { IErrorResponse } from 'src/app/share/models/error-message.model';
 import { CoreModule } from '../../core.module';
+import { LocalStoreService } from '../local-store.service';
 
 @Injectable({
   providedIn: CoreModule,
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private localStore: LocalStoreService) {}
 
   public signUp(user: IUser): Observable<IUser | IErrorResponse> {
     const url = `${BASEURL}/signup`;
@@ -18,9 +19,13 @@ export class AuthService {
     return this.http.post<IUser | IErrorResponse>(url, user, { headers });
   }
 
-  public signIn(user: IUser): Observable<IToken | IErrorResponse> {
+  public signIn(user: IUser): Observable<IToken> {
     const url = `${BASEURL}/signin`;
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<IToken | IErrorResponse>(url, user, { headers });
+    return this.http.post<IToken>(url, user, { headers }).pipe(
+      tap((value) => {
+        this.localStore.saveData(TOKEN_KEY, value.token);
+      }),
+    );
   }
 }
