2f4ce86c66fea975f64e5ae96f8611103ffa7d0d
Andronio <gprsboost03@mail.ru>
2022-11-01 19:12:30 +0600

    feat: add board service API

---
+++ project-management/src/app/share/models/column.model.ts
@@ -0,0 +1,12 @@
+import { ITask } from './task.model';
+
+export interface IColumn {
+  id: string;
+  title: string;
+  order: number;
+  tasks?: ITask[];
+}
+
+export interface IColumnDto {
+  title: string;
+}
