a9b72e3ed390cea70d831f50f8e448a0f7b27969
diana2886 <tdm2886@gmail.com>
2022-11-01 21:21:11 +0600

    feat: add footer component

--- project-management/src/app/core/core.module.ts
+++ project-management/src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import CoreRoutingModule from './core-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { ShareModule } from '../share/share.module';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, WelcomePageComponent],
+  declarations: [HeaderComponent, WelcomePageComponent, FooterComponent],
   imports: [CommonModule, CoreRoutingModule, ShareModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
