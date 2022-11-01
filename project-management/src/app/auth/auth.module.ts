9bd52c22f03bf65f2029db16fabfbce9cd3a92d8
Aibek <ai.bek@mail.ru>
2022-11-01 19:15:58 +0600

    fix: change import paths

--- project-management/src/app/auth/auth.module.ts
+++ project-management/src/app/auth/auth.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-
-
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule],
 })
-export class AuthModule { }
+export class AuthModule {}
