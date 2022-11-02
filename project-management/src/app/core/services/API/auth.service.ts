5ad4b1aa799172fd38dbed507fa2aae1c4d7d2d2
Aibek <ai.bek@mail.ru>
2022-11-02 11:53:55 +0600

    feat: add getToken method

--- project-management/src/app/core/services/API/auth.service.ts
+++ project-management/src/app/core/services/API/auth.service.ts
@@ -1,31 +1,36 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
-import { BASEURL, TOKEN_KEY } from 'src/app/share/constants/constants';
+import { TOKEN_KEY } from 'src/app/share/constants/constants';
 import { IToken, IUser } from 'src/app/share/models/auth.model';
 import { IErrorResponse } from 'src/app/share/models/error-message.model';
-import { CoreModule } from '../../core.module';
 import { LocalStoreService } from '../local-store.service';
 
 @Injectable({
-  providedIn: CoreModule,
+  providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient, private localStore: LocalStoreService) {}
 
   public signUp(user: IUser): Observable<IUser | IErrorResponse> {
-    const url = `${BASEURL}/signup`;
+    const endpoint = '/signup';
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<IUser | IErrorResponse>(url, user, { headers });
+    return this.http.post<IUser | IErrorResponse>(endpoint, user, { headers });
   }
 
-  public signIn(user: IUser): Observable<IToken> {
-    const url = `${BASEURL}/signin`;
+  public signIn(user: IUser): Observable<IToken | IErrorResponse> {
+    const endpoint = '/signin';
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<IToken>(url, user, { headers }).pipe(
+    return this.http.post<IToken | IErrorResponse>(endpoint, user, { headers }).pipe(
       tap((value) => {
-        this.localStore.saveData(TOKEN_KEY, value.token);
+        if ('token' in value) {
+          this.localStore.saveData(TOKEN_KEY, value.token);
+        }
       }),
     );
+  }
+
+  public getToken() {
+    return this.localStore.getData(TOKEN_KEY);
   }
 }
