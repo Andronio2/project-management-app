89ade807fb117fe58f69c7b9335595d89e01c4b1
Aibek <ai.bek@mail.ru>
2022-11-02 12:06:49 +0600

    feat: add checking authorization by valid token

--- project-management/src/app/core/services/API/auth.service.ts
+++ project-management/src/app/core/services/API/auth.service.ts
@@ -12,6 +12,10 @@ import { LocalStoreService } from '../local-store.service';
 export class AuthService {
   constructor(private http: HttpClient, private localStore: LocalStoreService) {}
 
+  public isAuth(): boolean {
+    return !!this.getToken();
+  }
+
   public signUp(user: IUser): Observable<IUser | IErrorResponse> {
     const endpoint = '/signup';
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
@@ -30,7 +34,16 @@ export class AuthService {
     );
   }
 
-  public getToken() {
-    return this.localStore.getData(TOKEN_KEY);
+  public getToken(): string | null {
+    const token: string | null = this.localStore.getData(TOKEN_KEY);
+    if (token) {
+      return this.isTokenExpired(token) ? token : null;
+    }
+    return null;
+  }
+
+  private isTokenExpired(token: string): boolean {
+    const expiry = JSON.parse(atob(token.split('.')[1])).exp;
+    return expiry * 1000 > Date.now();
   }
 }
