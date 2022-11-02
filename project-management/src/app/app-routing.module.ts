1c3c817c2a8a899c4cdef37dad3170274597df8d
Aibek <ai.bek@mail.ru>
2022-11-02 17:10:17 +0600

    feat: add base routing for auth pages

--- project-management/src/app/app-routing.module.ts
+++ project-management/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { RegisterComponent } from './auth/pages/register/register.component';
 import { WelcomePageComponent } from './core/pages/welcome-page/welcome-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'welcome', component: WelcomePageComponent },
+  { path: 'signin', component: LoginComponent },
+  { path: 'signup', component: RegisterComponent },
 ];
 
 @NgModule({
