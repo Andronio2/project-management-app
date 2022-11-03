cf088fd3fd5749bb9ca9040cc4ebaf8f28b2b547
Aibek <ai.bek@mail.ru>
2022-11-03 13:11:21 +0600

    feat: add sign in after sign up request

--- project-management/src/app/auth/pages/register/register.component.ts
+++ project-management/src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { tap } from 'rxjs/operators';
 import { AuthService } from 'src/app/core/services/API/auth.service';
+import { ISigninUserDto } from 'src/app/share/models/auth.model';
 import passwordValidator from '../../validators/passwordValidator';
 @Component({
   selector: 'app-register',
@@ -26,6 +28,19 @@ export class RegisterComponent {
 
   onSubmit(event: Event) {
     event.preventDefault();
-    this.authService.signUp(this.registerForm.getRawValue()).subscribe();
+    this.authService
+      .signUp(this.registerForm.getRawValue())
+      .pipe(
+        tap((value) => {
+          if ('login' in value) {
+            const userSign: ISigninUserDto = {
+              login: value.login,
+              password: this.password.value!,
+            };
+            this.authService.signIn(userSign).subscribe();
+          }
+        }),
+      )
+      .subscribe();
   }
 }
