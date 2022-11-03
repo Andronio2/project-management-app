6854731152373367d90c54798a478706f3d9c487
Aibek <ai.bek@mail.ru>
2022-11-03 16:42:10 +0600

    feat: set up app reducer and effects for store

--- project-management/src/app/app.module.ts
+++ project-management/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import AppEffects from './redux/effects/users.effects';
+import { appReducer } from './redux/reducers/app.reducer';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,8 +20,8 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
+    StoreModule.forRoot({ app: appReducer }, {}),
+    EffectsModule.forRoot([AppEffects]),
     StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
