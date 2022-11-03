66ee6ebf4ad907d612118ec655996f1ec4d2f8b7
Aibek <ai.bek@mail.ru>
2022-11-03 13:12:08 +0600

    feat: add sign in after submit login form

--- project-management/src/app/auth/pages/login/login.component.ts
+++ project-management/src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/API/auth.service';
+import { ISigninUserDto } from 'src/app/share/models/auth.model';
 import passwordValidator from '../../validators/passwordValidator';
 
 @Component({
@@ -14,10 +16,19 @@ export class LoginComponent {
 
   password = new FormControl('', [Validators.required, passwordValidator]);
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authService: AuthService) {
     this.loginForm = this.fb.group({
       login: this.login,
       password: this.password,
     });
+  }
+
+  logIn(event: Event) {
+    event.preventDefault();
+    const userSign: ISigninUserDto = {
+      login: this.login.value!,
+      password: this.password.value!,
+    };
+    this.authService.signIn(userSign).subscribe();
   }
 }
