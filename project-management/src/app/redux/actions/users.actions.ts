cd8888e642d5eeb709b52d23c40877f6202c749d
Aibek <ai.bek@mail.ru>
2022-11-03 16:43:40 +0600

    feat: add user actions for store

---
+++ project-management/src/app/redux/actions/users.actions.ts
@@ -0,0 +1,17 @@
+import { createAction, props } from '@ngrx/store';
+import { IUser } from 'src/app/share/models/auth.model';
+import { IErrorResponse } from 'src/app/share/models/error-message.model';
+
+export const getUsers = createAction('[User] GET_USERS');
+
+export const getUser = createAction('[User]  GET_EDIT_USER', props<{ id: string }>());
+
+export const getUsersSuccess = createAction(
+  '[User]  GET_USERS_SUCCESSES',
+  props<{ users: IUser[] | IErrorResponse }>(),
+);
+
+export const getUserSuccess = createAction(
+  '[User]  GET_EDIT_USER_SUCCESSES',
+  props<{ user: IUser | IErrorResponse }>(),
+);
