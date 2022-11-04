a1d0058fe06a05108377852a8d4848967a7435a2
Aibek <ai.bek@mail.ru>
2022-11-04 22:03:23 +0600

    feat: add create task modal

--- project-management/src/app/board/board.module.ts
+++ project-management/src/app/board/board.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CreateColumnComponent } from './components/create-column/create-column.component';
+import { ShareModule } from '../share/share.module';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CreateColumnComponent, CreateTaskComponent],
+  imports: [CommonModule, ShareModule],
 })
-export class BoardModule { }
+export class BoardModule {}
