e8fa1b1f13c356726810d3e5cf170cbe154ae372
diana2886 <tdm2886@gmail.com>
2022-11-04 17:49:52 +0600

    feat: add logic for language buttons

--- project-management/src/app/core/components/header/header.component.ts
+++ project-management/src/app/core/components/header/header.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  activeLang: string;
+
+  availableLang: string[] | { id: string; label: string }[];
+
+  constructor(private translateService: TranslocoService) {
+    this.activeLang = this.translateService.getActiveLang();
+    this.availableLang = this.translateService.getAvailableLangs();
+  }
+
+  changeLang(lang: string) {
+    this.translateService.setActiveLang(lang);
+    this.activeLang = lang;
+  }
+}
