36c31dfc0c17028da6519c8064c60eaf1bef0962
Aibek <ai.bek@mail.ru>
2022-11-07 11:08:13 +0600

    fix: add styles to page layouts

--- project-management/src/app/auth/pages/register/register.component.ts
+++ project-management/src/app/auth/pages/register/register.component.ts
@@ -5,6 +5,7 @@ import { ICreateUserDto, ISigninUserDto } from 'src/app/share/models/auth.model'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
   constructor(private authService: AuthService) {}
