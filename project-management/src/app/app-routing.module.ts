8421ef8c0121a20f44625e2e0923eed9c165de5a
diana2886 <tdm2886@gmail.com>
2022-11-07 12:59:16 +0600

    feat: add main module

--- project-management/src/app/app-routing.module.ts
+++ project-management/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './core/pages/welcome-page/welcome-page.component';
 import { BoardComponent } from './board/pages/board/board.component';
-import { BoardsListComponent } from './board/pages/boards-list/boards-list.component';
+import { BoardsListComponent } from './main/pages/boards-list/boards-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
   { path: 'board', component: BoardComponent }, // временный компонент для теста
   { path: 'board-list', component: BoardsListComponent }, // временный компонент для теста
+  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
 ];
 
 @NgModule({
