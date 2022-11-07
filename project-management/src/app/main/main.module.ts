6cd1f63b2a63a6ba32ba81248c7ab4bf0ebb9028
diana2886 <tdm2886@gmail.com>
2022-11-07 16:12:12 +0600

    feat: add main page translation

--- project-management/src/app/main/main.module.ts
+++ project-management/src/app/main/main.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import MainRoutingModule from './main-routing.module';
 import { BoardsListComponent } from './pages/boards-list/boards-list.component';
 import { ShareModule } from '../share/share.module';
+import { TranslocoRootModule } from '../transloco-root.module';
 
 @NgModule({
   declarations: [BoardsListComponent],
-  imports: [CommonModule, MainRoutingModule, ShareModule],
+  imports: [CommonModule, MainRoutingModule, ShareModule, TranslocoRootModule],
 })
 export class MainModule {}
