14bfac0b17a4c469b62b7aa46635d87ea447852d
diana2886 <tdm2886@gmail.com>
2022-11-07 12:58:30 +0600

    feat: replace boards component to main module

--- project-management/src/app/board/board.module.ts
+++ project-management/src/app/board/board.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColumnComponent } from './components/column/column.component';
 import { TaskComponent } from './components/task/task.component';
-import { BoardsListComponent } from './pages/boards-list/boards-list.component';
 import { BoardPreviewComponent } from './components/board-preview/board-preview.component';
 
 @NgModule({
-  declarations: [ColumnComponent, TaskComponent, BoardsListComponent, BoardPreviewComponent],
+  declarations: [ColumnComponent, TaskComponent, BoardPreviewComponent],
   imports: [CommonModule],
 })
 export class BoardModule {}
