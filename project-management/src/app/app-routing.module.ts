770e1c1fda47489455b6c8e3b6dc6fe6ea9f9adc
diana2886 <tdm2886@gmail.com>
2022-11-07 22:51:34 +0600

    feat: change welcome route to lazy loaded

--- project-management/src/app/app-routing.module.ts
+++ project-management/src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WelcomePageComponent } from './core/pages/welcome-page/welcome-page.component';
 import { BoardComponent } from './board/pages/board/board.component';
 import { BoardsListComponent } from './main/pages/boards-list/boards-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
-  { path: 'welcome', component: WelcomePageComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
+  { path: 'welcome', loadChildren: () => import('./core/core.module').then((m) => m.CoreModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
   { path: 'board', component: BoardComponent }, // временный компонент для теста
   { path: 'board-list', component: BoardsListComponent }, // временный компонент для теста
-  { path: 'main', loadChildren: () => import('./main/main.module').then((m) => m.MainModule) },
 ];
 
 @NgModule({
