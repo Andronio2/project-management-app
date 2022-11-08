31e294a9054cd2aac90bea6b1cb150167e55293c
diana2886 <tdm2886@gmail.com>
2022-11-08 17:48:22 +0600

    feat: add board-routing

--- project-management/src/app/board/board.module.ts
+++ project-management/src/app/board/board.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ColumnComponent } from './components/column/column.component';
 import { TaskComponent } from './components/task/task.component';
 import { BoardPreviewComponent } from './components/board-preview/board-preview.component';
+import BoardRoutingModule from './board-routing.module';
+import { BoardComponent } from './pages/board/board.component';
+import { ShareModule } from '../share/share.module';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
-  declarations: [ColumnComponent, TaskComponent, BoardPreviewComponent],
-  imports: [CommonModule],
+  declarations: [ColumnComponent, TaskComponent, BoardPreviewComponent, BoardComponent],
+  imports: [CommonModule, BoardRoutingModule, ShareModule, TranslocoModule],
 })
 export class BoardModule {}
