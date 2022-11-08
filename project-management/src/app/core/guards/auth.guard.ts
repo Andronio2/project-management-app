3e8f80acecc6c8eec47cbb3bc98cb44e7b5ea8db
Aibek <ai.bek@mail.ru>
2022-11-08 21:45:53 +0600

    feat: add canActivate to AuthGuard

--- project-management/src/app/core/guards/auth.guard.ts
+++ project-management/src/app/core/guards/auth.guard.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/API/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanLoad {
+export class AuthGuard implements CanLoad, CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
+  canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.authService.isAuth() || this.router.createUrlTree(['/welcome']);
+  }
   canLoad(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.isAuth() || this.router.createUrlTree(['/welcome']);
   }
