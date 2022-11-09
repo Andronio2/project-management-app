9698508ab542944316cc6324bb50868c4e184dca
diana2886 <tdm2886@gmail.com>
2022-11-09 18:17:18 +0600

    feat: move column logic from board component to column component

--- project-management/src/app/board/components/column/column.component.ts
+++ project-management/src/app/board/components/column/column.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModalService } from 'src/app/core/services/modal.service';
+import { ModalType } from 'src/app/share/constants/constants';
+import { IBoard } from 'src/app/share/models/board.model';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
-  styleUrls: ['./column.component.scss']
+  styleUrls: ['./column.component.scss'],
 })
-export class ColumnComponent implements OnInit {
+export class ColumnComponent {
+  @Input() board!: {
+    board$: Observable<IBoard | undefined>;
+    boardId: string;
+  };
 
-  constructor() { }
+  columnTitle = '';
 
-  ngOnInit(): void {
+  isEditColumnTitle = false;
+
+  constructor(private modalService: ModalService) {}
+
+  deleteColumn(id: string) {
+    this.modalService.openConfirmDelete(ModalType.COLUMN, this.board.boardId, id);
   }
 
+  createTask(columnId: string) {
+    this.modalService.openCreateMod(ModalType.CREATE, ModalType.TASK, this.board.boardId, columnId);
+  }
+
+  setEditMode() {
+    this.isEditColumnTitle = true;
+  }
 }
