c4576a98339c585d4c924621d6a11b849130f72b
diana2886 <tdm2886@gmail.com>
2022-11-09 18:17:46 +0600

    feat: move task logic from board component to task component

--- project-management/src/app/board/components/task/task.component.ts
+++ project-management/src/app/board/components/task/task.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ModalService } from 'src/app/core/services/modal.service';
+import { ModalType } from 'src/app/share/constants/constants';
+import { IColumn } from 'src/app/share/models/column.model';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.scss']
+  styleUrls: ['./task.component.scss'],
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent {
+  @Input() column!: IColumn;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  deleteTask(id: string) {
+
   }
-
 }
