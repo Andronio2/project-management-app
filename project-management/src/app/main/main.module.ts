bf5621b6840b38b6cb5b32cdf9d9e0b54ae965b5
diana2886 <tdm2886@gmail.com>
2022-11-13 23:48:02 +0600

    refactor: delete search pipe

--- project-management/src/app/main/main.module.ts
+++ project-management/src/app/main/main.module.ts
@@ -5,10 +5,9 @@ import MainRoutingModule from './main-routing.module';
 import { BoardsListComponent } from './pages/boards-list/boards-list.component';
 import { ShareModule } from '../share/share.module';
 import { TranslocoRootModule } from '../transloco-root.module';
-import { SearchBoardsPipe } from './pipes/search-boards.pipe';
 
 @NgModule({
-  declarations: [BoardsListComponent, SearchBoardsPipe],
+  declarations: [BoardsListComponent],
   imports: [CommonModule, MainRoutingModule, ShareModule, TranslocoRootModule],
 })
 export class MainModule {}
