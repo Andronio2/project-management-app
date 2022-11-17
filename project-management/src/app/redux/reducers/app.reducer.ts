69ccb022a6883ca2a5ecfcfec07bc6c55d901afc
diana2886 <tdm2886@gmail.com>
2022-11-17 23:45:27 +0600

    feat: add store settings for markTask

--- project-management/src/app/redux/reducers/app.reducer.ts
+++ project-management/src/app/redux/reducers/app.reducer.ts
@@ -1,9 +1,11 @@
 import { boardStatusReducer } from './board-stat.reducer';
 import { boardReducer } from './board.reducer';
+import { markTaskReducer } from './mark-task.reducer';
 import { userReducer } from './user.reducer';
 
 export const appReducer = {
   user: userReducer,
   board: boardReducer,
   boardLoaded: boardStatusReducer,
+  markTask: markTaskReducer,
 };
