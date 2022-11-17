0407ad8620ca37c346f04af55a535cf159ab48b4
Aibek <ai.bek@mail.ru>
2022-11-17 20:51:06 +0600

    feat: set up interceptor for loader

---
+++ project-management/src/app/core/services/API/loader-interceptor.service.ts
@@ -0,0 +1,19 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { finalize, Observable, tap } from 'rxjs';
+import { ProgressBarService } from '../progress-bar.service';
+
+@Injectable()
+export class LoaderInterceptorService implements HttpInterceptor {
+  constructor(private progressBar: ProgressBarService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.progressBar.show();
+    return next.handle(req).pipe(
+      tap({
+        error: () => this.progressBar.hide(),
+      }),
+      finalize(() => this.progressBar.hide())
+    );
+  }
+}
