991d63359af98ed8d4a0273cbe294c2d878def9f
diana2886 <tdm2886@gmail.com>
2022-11-17 11:32:17 +0600

    refactor: add some changes

--- project-management/src/app/core/components/header/header.component.ts
+++ project-management/src/app/core/components/header/header.component.ts
@@ -66,7 +66,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
         }
       });
 
-    this.progressBarService.show();
+    // this.progressBarService.show();
   }
 
   ngOnDestroy(): void {
